4ee5544fdb211f1c6bd09934e025ab3c1b73160e
Kipcammy <cameron_zoe@hotmail.com>
2017-09-29 13:14:34 +0200

    2.0inlog

--- root/public/user/logprotocol/login.php
+++ root/public/user/logprotocol/login.php
@@ -1,37 +1,38 @@
+<!DOCTYPE html>
 <html>
 //login script
 //check of gebruiker is ingelogd
 //store cookie
+<?php include_once("./../../../template_php/template_head.php")?>
+<?php include_once("./../../../template_php/template_header.php")?>
 
-<?php>
-echo "
-<div id="login, form">
-<form>
-
-<h1>
-
-Username/e-mail: <br>
-<input type=' "test" name = "login" value = "Henk"><br>
-Password:<br>
-<input type ="password" name= "pass" ><br>
-Submit<br>
-<input type = "submit" value = "submit"
-
-</h1>
-
-
-</form>
-<input type = "submit" value "geen_account"<br>
-<a></a>
-
-
-</div>
-
-
-"
+    <div id="login, form">
+        <form>
+            
+            <h1>login</h1>
+            
+            Username/e-mail: <br>
+            <input type= "text" name = "login" value = "Henk"><br>
+            Password:<br>
+            <input type ="password" name= "pass" ><br>
+            Submit<br>
+            <input type = "submit" value = "submit" ><br>
+            
+            
+        
+        </form>
+        <a href="./register.php">Register</a><br>
+        <a href="./register.php">Forgot Password?</a>
+        
+        
+    </div>
 
 
 
-?>
+
+
+
+
+<?php include_once("./../../../template_php/template_footer.php")?>
 
 </html>
