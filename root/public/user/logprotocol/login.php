7010dc98b919478f803cb869d3d37e9061b1fdf9
Kipcammy <cameron_zoe@hotmail.com>
2017-09-29 12:58:18 +0200

    inlog

--- root/public/user/logprotocol/login.php
+++ root/public/user/logprotocol/login.php
@@ -1,3 +1,37 @@
+<html>
 //login script
 //check of gebruiker is ingelogd
 //store cookie
+
+<?php>
+echo "
+<div id="login, form">
+<form>
+
+<h1>
+
+Username/e-mail: <br>
+<input type=' "test" name = "login" value = "Henk"><br>
+Password:<br>
+<input type ="password" name= "pass" ><br>
+Submit<br>
+<input type = "submit" value = "submit"
+
+</h1>
+
+
+</form>
+<input type = "submit" value "geen_account"<br>
+<a></a>
+
+
+</div>
+
+
+"
+
+
+
+?>
+
+</html>
